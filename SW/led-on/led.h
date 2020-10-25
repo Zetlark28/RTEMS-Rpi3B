15e4381d4eeb77d3ad4aa613bb3611804ab14b87
Zetlark28 <clarke2897@gmail.com>
2020-10-25 13:06:21 +0100

    edited led-on sources file

--- SW/led-on/led.h
+++ SW/led-on/led.h
@@ -8,13 +8,15 @@
 
 #define INP_GPIO(g) *(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
 #define OUT_GPIO(g) *(gpio+((g)/10)) |=  (1<<(((g)%10)*3))
+#define GET_GPIO(g) (*(gpio+13)&(1<<g)) // 0 if LOW, (1<<g) if HIGH
 
-#define GPIO_SET_EXT *(gpio+8)  // sets   bits which are 1 ignores bits which are 0
-#define GPIO_CLR_EXT *(gpio+11) // clears bits which are 1 ignores bits which are 0
+#define GPIO_SET *(gpio+8)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio+11) // clears bits which are 1 ignores bits which are 0
 
 //47 = GPIO PIN 15
-#define LED_INIT()  do { unsigned int *gpio = (unsigned int *)BCM2835_GPIO_REGS_BASE; OUT_GPIO(15);} while(0)
-#define LED_ON()  do { unsigned int *gpio = (unsigned int *)BCM2835_GPIO_REGS_BASE; GPIO_CLR_EXT = 1 << (15 % 32);} while(0)
-#define LED_OFF()  do { unsigned int *gpio = (unsigned int *)BCM2835_GPIO_REGS_BASE; GPIO_SET_EXT = 1 << (15 % 32);} while(0)
+#define LED_INIT(g)  do { unsigned int *gpio = (unsigned int *)BCM2835_GPIO_REGS_BASE; INP_GPIO(g);OUT_GPIO(g);} while(0)
+#define LED_ON(g)  do { unsigned int *gpio = (unsigned int *)BCM2835_GPIO_REGS_BASE; GPIO_CLR = 1 << (g % 32);} while(0)
+#define LED_OFF(g)  do { unsigned int *gpio = (unsigned int *)BCM2835_GPIO_REGS_BASE; GPIO_SETs = 1 << (g % 32);} while(0)
 
+int led_status(int g){do{unsigned int *gpio = (unsigned int *) BCM2835_GPIO_REGS_BASE; return GET_GPIO(g);}while(0);}
 #endif //LED_TEST_LED_H
--- SW/led-on/main.c
+++ SW/led-on/main.c
@@ -7,9 +7,11 @@
 #include "led.h"
 
 rtems_task Init(rtems_task_argument argument){
-  printf( "\n\n*** LED (GPIO15) ON ***" );
+  printf( "\n\n*** LED (GPIO4) ON ***" );
   fflush(stdout);
-  LED_INIT();
-  LED_ON();
+  LED_INIT(4);
+  LED_ON(4);
+  printf("\n\n *** FINISH TEST  *** ");
+  fflush(stdout);
   exit(0);
 }
