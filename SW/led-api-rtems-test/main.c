6fed7618fe8b81f275a9560810573544bf1c4259
Zetlark28 <clarke2897@gmail.com>
2020-10-30 08:38:15 +0100

    fix led-api-rtems-tes

--- SW/led-api-rtems-test/main.c
+++ SW/led-api-rtems-test/main.c
@@ -1,28 +1,34 @@
 #include <rtems.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <bsp/mmu.h>
-#include <inttypes.h>
 #include <bsp/raspberrypi.h>
 #include <bsp/gpio.h>
 #include <bsp/rpi-gpio.h>
-#include "led.h"
+#include <inttypes.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 rtems_task Init(rtems_task_argument argument){
 
-  printf("***START TEST*** \n");
-  fflush(stdout);
-  bsp_start();
+  void *bsp_spec;
   rtems_interval  seconds = 1 * rtems_clock_get_ticks_per_second();
   rtems_gpio_initialize();
-  rtems_task_wake_after(seconds);
-  rtems_gpio_bsp_clear(0,4);
-  rtems_task_wake_after(seconds);
-  rtems_gpio_bsp_set(0,4);
-  rtems_task_wake_after(seconds);
-  rtems_gpio_bsp_clear(0,4);
+  rtems_gpio_bsp_select_input(0, 4, &bsp_spec);
+  rtems_gpio_bsp_select_output(0, 4, &bsp_spec);
+  printf("***START TEST*** \n");
+  fflush(stdout);
+
+  while(1){
+
+	  if(rtems_gpio_bsp_get_value(0, 4)){
+		  rtems_gpio_bsp_clear(0,4);
+	  }else{
+		  rtems_gpio_bsp_set(0,4);
+	  }
+	  rtems_task_wake_after(seconds);
+  }
+
+  //should reach here
   printf("***FINISH TEST***\n");
   fflush(stdout);
-  rtems_task_wake_after(seconds);
   exit(0);
 }
