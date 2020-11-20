b33239c9ee54ee91896cb6c10128b59d67083936
Zetlark28 <clarke2897@gmail.com>
2020-11-20 07:26:45 +0100

    modified TODO

--- SW/led-api-rtems-test/int.c
+++ SW/led-api-rtems-test/int.c
@@ -10,21 +10,22 @@
 
 rtems_task Init(rtems_task_argument argument){
 
+  int pin = 4	
   void *bsp_spec;
   rtems_interval  seconds = 1 * rtems_clock_get_ticks_per_second();
   rtems_gpio_initialize();
-  rtems_gpio_bsp_select_input(0, 4, &bsp_spec);
-  rtems_gpio_bsp_select_output(0, 4, &bsp_spec);
+  rtems_gpio_bsp_select_input(0, pin, &bsp_spec);
+  rtems_gpio_bsp_select_output(0, pin, &bsp_spec);
 
   printf("***START TEST*** \n");
   fflush(stdout);
 
   while(1){
 
-	  if(rtems_gpio_bsp_get_value(0, 4)){
-		  rtems_gpio_bsp_clear(0,4);
+	  if(rtems_gpio_bsp_get_value(0, pin)){
+		  rtems_gpio_bsp_clear(0,pin);
 	  }else{
-		  rtems_gpio_bsp_set(0,4);
+		  rtems_gpio_bsp_set(0,pin);
 	  }
 	  rtems_task_wake_after(seconds);
   }
